d42fe37bca0bb1c9dbd1fba92fe787fd2b74addb
Saideep Kumar <saideep518@gmail.com>
2026-03-11 01:45:40 +0530

    Update CustomerRepo.java

--- app/src/main/java/org/example/repository/CustomerRepo.java
+++ app/src/main/java/org/example/repository/CustomerRepo.java
@@ -2,7 +2,7 @@ package org.example.repository;
 
 import org.example.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Repository; 
 
 import java.util.Optional;
 
