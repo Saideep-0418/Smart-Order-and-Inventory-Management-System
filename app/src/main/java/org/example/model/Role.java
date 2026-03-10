c0d52645e752049c521480794cb901875f12d42e
Saideep Kumar <saideep518@gmail.com>
2026-03-11 01:45:14 +0530

    Update Role.java

--- app/src/main/java/org/example/model/Role.java
+++ app/src/main/java/org/example/model/Role.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 @Entity
 public class Role {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
     private long id;
     private String name;
 
