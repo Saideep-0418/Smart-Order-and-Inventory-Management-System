386042594e27297362036c9c848ef2665b29c804
Saideep Kumar <saideep518@gmail.com>
2026-03-11 01:44:40 +0530

    Update Order.java

--- app/src/main/java/org/example/model/Order.java
+++ app/src/main/java/org/example/model/Order.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.util.ArrayList; 
 import java.util.List;
 
 @Entity
