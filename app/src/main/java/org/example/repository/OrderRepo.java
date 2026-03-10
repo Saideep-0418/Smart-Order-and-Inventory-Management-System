2939de5b9c3e01dc784f460fba8e81d5e0a2dc15
Saideep Kumar <saideep518@gmail.com>
2026-03-11 01:46:22 +0530

    Update OrderRepo.java

--- app/src/main/java/org/example/repository/OrderRepo.java
+++ app/src/main/java/org/example/repository/OrderRepo.java
@@ -6,7 +6,7 @@ import org.example.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.List; 
 
 @Repository
 public interface OrderRepo extends JpaRepository<Order,Long> {
