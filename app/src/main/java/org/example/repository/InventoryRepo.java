c2e805ecb85eba5f0200fd733223aa46490c2ba1
Saideep Kumar <saideep518@gmail.com>
2026-03-11 01:45:50 +0530

    Update InventoryRepo.java

--- app/src/main/java/org/example/repository/InventoryRepo.java
+++ app/src/main/java/org/example/repository/InventoryRepo.java
@@ -4,7 +4,7 @@ import org.example.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.Optional; 
 
 @Repository
 public interface InventoryRepo extends JpaRepository<Inventory,Long> {
