674fabe1d2b35fc6b0127c07ff15c59ed4f10d39
Saideep Kumar <saideep518@gmail.com>
2026-03-11 01:40:54 +0530

    Update RoleService.java

--- app/src/main/java/org/example/Service/RoleService.java
+++ app/src/main/java/org/example/Service/RoleService.java
@@ -2,7 +2,7 @@ package org.example.Service;
 
 import org.example.model.Role;
 import java.util.Optional;
-
+ 
 public interface RoleService {
     Role createRole(Role role);
     Optional<Role> getRoleByName(String roleName);
