b1afca8cf03a8d91c2478a1ee2ff0e78416f3a4e
Saideep Kumar <saideep518@gmail.com>
2026-03-11 01:41:45 +0530

    Update JWTFilter.java

--- app/src/main/java/org/example/config/JWTFilter.java
+++ app/src/main/java/org/example/config/JWTFilter.java
@@ -3,7 +3,7 @@ package org.example.config;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletResponse; 
 import org.example.Service.JwtService;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
