7bab9fef01300039566223c60a1a0403405cfea3
Saideep Kumar <saideep518@gmail.com>
2026-03-11 01:38:39 +0530

    Update InventoryServiceImp.java

--- app/src/main/java/org/example/Service/InventoryServiceImp.java
+++ app/src/main/java/org/example/Service/InventoryServiceImp.java
@@ -6,7 +6,7 @@ import org.example.repository.InventoryRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.List; 
 
 @Service
 public class InventoryServiceImp implements InventoryService{
