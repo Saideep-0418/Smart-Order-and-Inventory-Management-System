8284f5a3d4d9af482e2a282a754c95e661e18e16
Saideep Kumar <saideep518@gmail.com>
2026-03-11 01:46:12 +0530

    Update OrderItemRepo.java

--- app/src/main/java/org/example/repository/OrderItemRepo.java
+++ app/src/main/java/org/example/repository/OrderItemRepo.java
@@ -5,7 +5,7 @@ import org.example.model.OrderItem;
 import org.example.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+ 
 import java.util.List;
 @Repository
 public interface OrderItemRepo extends JpaRepository<OrderItem,Long> {
