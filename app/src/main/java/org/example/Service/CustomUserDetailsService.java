a926fa816d2c809137d85a9704df74b068bc37dd
Saideep Kumar <saideep518@gmail.com>
2026-03-11 01:37:51 +0530

    Update CustomUserDetailsService.java

--- app/src/main/java/org/example/Service/CustomUserDetailsService.java
+++ app/src/main/java/org/example/Service/CustomUserDetailsService.java
@@ -9,7 +9,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CustomUserDetailsService implements UserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService { 
 
     private final UserRepo userRepo;
 
