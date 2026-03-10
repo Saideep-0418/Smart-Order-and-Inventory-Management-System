cd9d3e4255f45ce0372679b47a546141e6b61803
Saideep Kumar <saideep518@gmail.com>
2026-03-11 01:47:01 +0530

    Update App.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -7,7 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
+@SpringBootApplication 
 @EnableAsync
 public class App {
     public static void main(String[] args) {
