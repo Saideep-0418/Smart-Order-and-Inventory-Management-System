f1f43cc9d00cd6f95e063cd6763b11f6fae4508c
Saideep Kumar <saideep518@gmail.com>
2026-03-11 01:41:04 +0530

    Update RoleServiceImp.java

--- app/src/main/java/org/example/Service/RoleServiceImp.java
+++ app/src/main/java/org/example/Service/RoleServiceImp.java
@@ -2,7 +2,7 @@ package org.example.Service;
 
 import org.example.model.Role;
 import org.example.repository.RoleRepo;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired; 
 import org.springframework.stereotype.Service;
 
 import java.util.List;
