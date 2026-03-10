fef23cbc07ebee8e1e9e4fbf1cdd7372b3c57499
Saideep Kumar <saideep518@gmail.com>
2026-03-11 01:45:24 +0530

    Update User.java

--- app/src/main/java/org/example/model/User.java
+++ app/src/main/java/org/example/model/User.java
@@ -6,7 +6,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private String username;
+    private String username; 
     private String password;
 
     @ManyToOne(fetch = FetchType.EAGER)
