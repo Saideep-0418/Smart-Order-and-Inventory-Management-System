62c62ea8c993f70b0d3424bdb206cc36a7beef2c
Saideep Kumar <saideep518@gmail.com>
2026-03-11 01:40:33 +0530

    Update ProductService.java

--- app/src/main/java/org/example/Service/ProductService.java
+++ app/src/main/java/org/example/Service/ProductService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ProductService {
-    Product createProduct(Product product);
+    Product createProduct(Product product); 
     List<Product> getAllProducts();
     Optional<Product> getProductById(long id);
     Optional<Product> getProductBySku(String sku);
